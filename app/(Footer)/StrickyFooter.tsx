6390e71a3d59ad066c18cfd36fdd595629401002
usersfro <palvaiuser@gmail.com>
2025-03-03 19:20:26 +0530

    added

--- app/(Footer)/StrickyFooter.tsx
+++ app/(Footer)/StrickyFooter.tsx
@@ -1,25 +1,25 @@
-"use client"
+"use client";
 
-import React, { useRef, useEffect, useState } from 'react';
-import { StickyBottomFooter } from '@/app/(Footer)/FooterData';
+import React, { useRef, useEffect, useState } from "react";
+import { StickyBottomFooter } from "@/app/(Footer)/FooterData";
 
 export default function Footer() {
   const contentRef = useRef<HTMLDivElement>(null);
   const [contentHeight, setContentHeight] = useState(310);
-  
+
   useEffect(() => {
     if (contentRef.current) {
       const currentRef = contentRef.current;
-      const resizeObserver = new ResizeObserver(entries => {
+      const resizeObserver = new ResizeObserver((entries) => {
         for (const entry of entries) {
           const height = entry.contentRect.height;
           const minHeight = window.innerWidth >= 768 ? 410 : 310;
           setContentHeight(Math.max(height, minHeight));
         }
       });
-      
+
       resizeObserver.observe(currentRef);
-      
+
       return () => {
         resizeObserver.unobserve(currentRef);
       };
@@ -31,19 +31,27 @@ export default function Footer() {
       className="relative"
       style={{
         height: `${contentHeight}px`,
-        clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)"
+        clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)",
       }}
     >
-      <div className="relative" style={{
-         height: `calc(100vh + ${contentHeight}px)`,
-        top: "-100vh"
-       }}>
+      <div
+        className="relative"
+        style={{
+          height: `calc(100vh + ${contentHeight}px)`,
+          top: "-100vh",
+        }}
+      >
         <div
           ref={contentRef}
           className="sticky"
           style={{
-            minHeight: typeof window !== 'undefined' ? (window.innerWidth >= 768 ? "410px" : "330px") : "330px",
-            top: `calc(100vh - ${contentHeight}px)`
+            minHeight:
+              typeof window !== "undefined"
+                ? window.innerWidth >= 768
+                  ? "410px"
+                  : "330px"
+                : "330px",
+            top: `calc(100vh - ${contentHeight}px)`,
           }}
         >
           <StickyBottomFooter />
@@ -52,8 +60,6 @@ export default function Footer() {
     </div>
   );
 }
-
-
 
 // import React from 'react'
 
@@ -64,7 +70,7 @@ export default function Footer() {
 //     //h- 410
 //     // 350
 
-//     <div 
+//     <div
 //         className='relative h-[410px]'
 //         style={{clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)"}}
 //     >
@@ -76,5 +82,3 @@ export default function Footer() {
 //     </div>
 //   )
 // }
-
-
