3f9771098ba5e23c5abcd2286c91c0d20037e0c4
usersfro <palvaiuser@gmail.com>
2025-03-03 19:01:08 +0530

    added

--- app/(Footer)/StrickyFooter.tsx
+++ app/(Footer)/StrickyFooter.tsx
@@ -1,25 +1,56 @@
-
-
-import React from 'react'
-
+"use client"
+import React, { useRef, useEffect, useState } from 'react';
 import { StickyBottomFooter } from '@/app/(Footer)/FooterData';
 
 export default function Footer() {
-  return (
-    //h- 410
-    // 350
+  const contentRef = useRef(null);
+  const [contentHeight, setContentHeight] = useState(310);
+  
+  useEffect(() => {
+    if (contentRef.current) {
+      const resizeObserver = new ResizeObserver(entries => {
+        for (let entry of entries) {
+          const height = entry.contentRect.height;
+          const minHeight = window.innerWidth >= 768 ? 410 : 310;
+          setContentHeight(Math.max(height, minHeight));
+        }
+      });
+      
+      resizeObserver.observe(contentRef.current);
+      
+      return () => {
+        if (contentRef.current) {
+          resizeObserver.unobserve(contentRef.current);
+        }
+      };
+    }
+  }, []);
 
-    <div 
-        className='relative h-[350px] md:h-[410px]'
-        style={{clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)"}}
+  return (
+    <div
+      className="relative"
+      style={{
+        height: `${contentHeight}px`,
+        clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)"
+      }}
     >
-        <div className='relative  h-[calc(100vh+410px)] -top-[100vh]'>
-            <div className='h-[410px] sticky top-[calc(100vh-410px)]'>
-               <StickyBottomFooter/>
-            </div>
+      <div className="relative" style={{ 
+        height: `calc(100vh + ${contentHeight}px)`,
+        top: "-100vh" 
+      }}>
+        <div 
+          ref={contentRef}
+          className="sticky" 
+          style={{ 
+            minHeight: window.innerWidth >= 768 ? "410px" : "330px",
+            top: `calc(100vh - ${contentHeight}px)`
+          }}
+        >
+          <StickyBottomFooter />
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
 
