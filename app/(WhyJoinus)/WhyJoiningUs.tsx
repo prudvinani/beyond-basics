1161e53b7f3122cbf6d3a50d7766cb6f9a559238
usersfro <palvaiuser@gmail.com>
2025-03-06 12:26:31 +0530

    added

--- app/(WhyJoinus)/WhyJoiningUs.tsx
+++ app/(WhyJoinus)/WhyJoiningUs.tsx
@@ -20,7 +20,7 @@ export  function JoinUsSection() {
             <div className="absolute -top-10 left-8 bg-[#966fd6] rounded-full p-2 w-[64px] h-[64px] flex items-center justify-center">
             <Image src={search} alt="search"/>
             </div>
-            <h2 className="text-[#12100e] text-[24px] font-medium mb-1 font-outfit  tracking-normal  ">Career-Boosting Insights</h2>
+            <h2 className="text-[#12100e] text-[20px] md:text-[24px] font-medium mb-1 font-outfit  tracking-normal  ">Career-Boosting Insights</h2>
             <p className="text-[#12100e] text-[18px] font-thin leading-6  ">
               Gain knowledge that enhances your career prospects and keeps you competitive.
             </p>
@@ -32,7 +32,7 @@ export  function JoinUsSection() {
               <Image src={start} alt="search"/>
               </div>
             </div>
-            <h2 className="text-[#12100e] text-[24px] font-medium mb-1">Tailored Learning Paths</h2>
+            <h2 className="text-[#12100e] text-[20px] md:text-[24px] font-medium mb-1">Tailored Learning Paths</h2>
             <p className="text-[#12100e] text-[18px] leading-6  ">
               Follow structured courses designed to meet your unique learning needs and goals.
             </p>
@@ -41,7 +41,7 @@ export  function JoinUsSection() {
           <div className="bg-white rounded-[24px] px-12 pt-10 mt-8 md:mt-0  relative h-[210px]  md:w-[350px] shadow-sm">
             <div className="absolute -top-10 left-8 bg-[#0165fc] rounded-full p-3  w-[64px] h-[64px]  flex items-center justify-center">
             <Image src={eye} alt="search"/>            </div>
-            <h2 className="text-[#12100e] text-[24px] font-medium mb-1">Stay ahead of the curve</h2>
+            <h2 className="text-[#12100e] text-[20px] md:text-[24px] font-medium mb-1">Stay ahead of the curve</h2>
             <p className="text-[#12100e] text-[18px]  leading-6   ">
               Learn the latest industry trends and emerging technologies to stay relevant.
             </p>
@@ -53,7 +53,7 @@ export  function JoinUsSection() {
               <Image src={heart} alt="search"/>
               </div>
             </div>
-            <h2 className="text-[#12100e] text-[24px] font-medium mb-3">Engaging Community</h2>
+            <h2 className="text-[#12100e] text-[20px] md:text-[24px] font-medium mb-3">Engaging Community</h2>
             <p className="text-[#12100e] text-[18px] leading-6 ">
               Connect with peers, mentors, and professionals for guidance and motivation.
             </p>
@@ -63,7 +63,7 @@ export  function JoinUsSection() {
             <div className="absolute -top-10 left-8 bg-[#ffbf00] rounded-full p-3 w-[64px] h-[64px]  flex items-center justify-center">
             <Image src={global} alt="search"/>
             </div>
-            <h2 className="text-[#12100e] text-[24px] font-medium mb-1">Real-World Applications</h2>
+            <h2 className="text-[#12100e] text-[18px] md:text-[24px] font-medium mb-1">Real-World Applications</h2>
             <p className="text-[#12100e] text-[18px] leading-6   ">
               Apply what you learn through practical projects and real-life scenarios.
             </p>
@@ -75,7 +75,7 @@ export  function JoinUsSection() {
               <Image src={student} alt="search"/>
               </div>
             </div>
-            <h2 className="text-[#12100e] text-[24px] font-medium mb-1">Industry Curriculum</h2>
+            <h2 className="text-[#12100e] text-[20px] md:text-[24px] font-medium mb-1">Industry Curriculum</h2>
             <p className="text-[#12100e] text-[18px]  leading-6      ">
               Access up-to-date content curated by experts to match industry standards.
             </p>
